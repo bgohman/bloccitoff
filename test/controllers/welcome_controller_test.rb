37a04223a30212c5f3960d0b67d331aa9e6a52ef
Bradley Gohman <bradley.s.gohman@gmail.com>
2015-06-02 16:37:49 -0500

    flesh out the home page

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -1,14 +1,17 @@
 require 'test_helper'
 
 class WelcomeControllerTest < ActionController::TestCase
+
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Blocitoff"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About | Blocitoff"
   end
 
 end
