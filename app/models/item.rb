7dc59b7c484972a8403f2dcff717a1bfe561daea
Bradley Gohman <bradley.s.gohman@gmail.com>
2015-06-14 13:03:31 -0500

    add days remaining display for items

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,8 @@ class Item < ActiveRecord::Base
   default_scope -> { order(:created_at) }
   validates :user_id, presence: true
   validates :name, presence: true
+
+  def days_left
+    7 - (DateTime.now.to_date - created_at.to_date).to_i
+  end
 end
